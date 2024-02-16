274953fbf06b031afe9a7ed7bc0d64c97d2b832f
Osamah Al-maliki <osamah.al-maliki@no.experis.com>
2024-02-16 14:29:11 +0100

    Finished core and extension task, and tested it with docker

---
+++ src/main/java/com/booleanuk/api/model/Course.java
@@ -0,0 +1,35 @@
+package com.booleanuk.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "start_date")
+    private String startDate;
+
+    @OneToMany(mappedBy = "course")
+    @JsonIgnore
+    private List<Student> students;
+}
